7e852306471dd5e890181991b85b518bb3678696
laipuni <2tgb02023@gmail.com>
2025-03-26 20:20:34 +0900

    [back] feat : 로그인 성공 핸들러 추가

--- back/src/main/java/com/example/cpsplatform/security/handler/CustomAuthenticationSuccessHandler.java
+++ back/src/main/java/com/example/cpsplatform/security/handler/CustomAuthenticationSuccessHandler.java
@@ -3,14 +3,25 @@ package com.example.cpsplatform.security.handler;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CsrfToken;
 
 import java.io.IOException;
 
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(final HttpServletRequest request, final HttpServletResponse response, final Authentication authentication) throws IOException, ServletException {
+        //todo 계정의 실패횟수 초기화 필요
 
+        // CSRF 토큰을 클라이언트로 응답 헤더에 추가
+        CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+        if (csrfToken != null) {
+            response.setHeader("X-CSRF-TOKEN", csrfToken.getToken());  // 응답 헤더에 CSRF 토큰 전달
+        }
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_OK);
     }
 }
