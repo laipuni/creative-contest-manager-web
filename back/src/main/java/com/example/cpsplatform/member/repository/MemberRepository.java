c87c3f21a74a8f26094d8ee6b7d812977ae3f279
laipuni <2tgb02023@gmail.com>
2025-03-26 18:33:54 +0900

    [back] feat : MemberRepository 추가

---
+++ back/src/main/java/com/example/cpsplatform/member/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.example.cpsplatform.member.repository;
+
+import com.example.cpsplatform.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+
+    Optional<Member> findMemberByLoginId(String loginId);
+
+}
