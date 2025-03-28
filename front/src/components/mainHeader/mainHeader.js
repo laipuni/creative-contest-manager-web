c8e3c8198e9457cd35eb8fba487ed416c34b15a7
pyunye <dpwns2318@naver.com>
2025-03-28 17:34:01 +0900

    [front] feat : mainHeader 컴포넌트 틀 구현

---
+++ front/src/components/mainHeader/mainHeader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./mainHeader.css"
+import "../../fonts/BMHANNAPro.ttf"
+import "../../fonts/Roboto-Bold.ttf"
+import "../../fonts/Roboto-Regular.ttf"
+import "../../fonts/Roboto-Medium.ttf"
+
+const MainHeader = () => {
+    return (
+        <div className="main-header-container">
+            <div className="main-header-top">
+                <div className="main-header-bot"></div>
+            </div>
+        </div>
+    )
+}
+
+export default MainHeader;
