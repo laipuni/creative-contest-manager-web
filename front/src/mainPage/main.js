f3a766c5e98924e929df91244fe7cd24925eebdf
pyunye <dpwns2318@naver.com>
2025-03-30 18:48:37 +0900

    [front] design : mainPage - 벤토 디자인 틀 구현

--- front/src/mainPage/main.js
+++ front/src/mainPage/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import MainHeader from "../components/mainHeader/mainHeader";
-//import MainHeader from 'src/components/mainHeader/mainHeader';
+import "./main.css"
 
 const Main = () => {
     return (
@@ -8,7 +8,27 @@ const Main = () => {
             <div className="main-content-container">
                 <MainHeader />
                 <div className="main-content">
-                    <h3>안녕하세요. 메인페이지 입니다.</h3>
+                    <div className="main-content-bento">
+                        <div className="bento-top">
+                            <div className="bento-top-content">
+                                <p>hi</p>
+                            </div>
+                            <div className="bento-top-content" style={{background: "rgba(89, 167, 255, 0.36)"}}></div>
+                        </div>
+                        <div className="bento-bot">
+                            <div className="bento-bot-left">
+                                <div className="bento-bot-left-upper">
+                                    <p>hi</p>
+                                </div>
+                                <div className="bento-bot-left-lower">
+                                    <p>hi</p>
+                                </div>
+                            </div>
+                            <div className="bento-bot-right">
+                                <p>hi</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
