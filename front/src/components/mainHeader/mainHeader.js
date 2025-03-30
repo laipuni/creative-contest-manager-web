9638871ba065403aa54fc571347f523031ec1eb1
pyunye <dpwns2318@naver.com>
2025-03-30 12:48:10 +0900

    [front] design : mainHeader 윗 부분 구현

--- front/src/components/mainHeader/mainHeader.js
+++ front/src/components/mainHeader/mainHeader.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import "./mainHeader.css"
-import "../../fonts/BMHANNAPro.ttf"
-import "../../fonts/Roboto-Bold.ttf"
-import "../../fonts/Roboto-Regular.ttf"
-import "../../fonts/Roboto-Medium.ttf"
-
+import './mainHeader.css'
+import '../../styles/styles.css'
+import { Link } from 'react-router-dom'
 const MainHeader = () => {
     return (
         <div className="main-header-container">
             <div className="main-header-top">
-                <div className="main-header-bot"></div>
+                <div className="main-header-menu">
+                    <div className="main-header-menu-item">
+                        <Link to="/" className="main-header-menu-item-text">HOME</Link>
+                        <div className="main-header-menu-item-line"></div>
+                    </div>
+                    <div className="main-header-menu-item">
+                        <p className="main-header-menu-item-text">LOGIN</p>
+                        <div className="main-header-menu-item-line"></div>
+                    </div>
+                    <div className="main-header-menu-item">
+                        <p className="main-header-menu-item-text">JOIN</p>
+                        <div className="main-header-menu-item-line"></div>
+                    </div>
+                    <div className="main-header-menu-item">
+                        <p className="main-header-menu-item-text">ADMIN</p>
+                    </div>
+                </div>
             </div>
+            <div className="main-header-bot"></div>
+            <div className="main-header-line"></div>
         </div>
     )
 }
