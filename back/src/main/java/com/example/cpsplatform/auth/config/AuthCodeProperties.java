2bf998fad561e871468952da2f037db1fa751e6b
laipuni <2tgb02023@gmail.com>
2025-04-02 13:14:39 +0900

    [back] feat : 인증 코드 전송, 검증을 역할 AuthService 추가

---
+++ back/src/main/java/com/example/cpsplatform/auth/config/AuthCodeProperties.java
@@ -0,0 +1,14 @@
+package com.example.cpsplatform.auth.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "auth.authcode")
+public class AuthCodeProperties {
+    long timeout = 5; // 기본값 설정 가능
+}
---
+++ back/src/main/java/com/example/cpsplatform/auth/config/AuthConfig.java
@@ -0,0 +1,73 @@
+package com.example.cpsplatform.auth.config;
+
+import com.example.cpsplatform.auth.AuthService;
+import com.example.cpsplatform.auth.email.EmailService;
+import com.example.cpsplatform.auth.generator.AuthCodeGenerator;
+import com.example.cpsplatform.auth.generator.UUIDAuthCodeGenerator;
+import com.example.cpsplatform.auth.sender.AuthCodeSender;
+import com.example.cpsplatform.auth.sender.EmailAuthCodeSender;
+import com.example.cpsplatform.auth.storage.AuthCodeStorage;
+import com.example.cpsplatform.auth.storage.RedisAuthCodeStorage;
+import com.example.cpsplatform.auth.strategy.AuthCodeStrategy;
+import com.example.cpsplatform.auth.strategy.RegisterAuthCodeStrategy;
+import com.example.cpsplatform.redis.RedisRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class AuthConfig {
+
+    public static final String REGISTER_AUTH = "register";
+
+    @Autowired
+    RedisRepository redisRepository;
+
+    @Autowired
+    EmailService emailService;
+
+    @Autowired
+    SpringTemplateEngine springTemplateEngine;
+
+    @Autowired
+    AuthCodeProperties authCodeProperties;
+
+    @Bean
+    public AuthService authService(){
+        return new AuthService(
+                authCodeSender(),
+                authCodeStorage(),
+                authCodeStrategy(),
+                authCodeGenerator()
+        );
+    }
+
+    @Bean
+    public Map<String,AuthCodeStrategy> authCodeStrategy(){
+        Map<String, AuthCodeStrategy> strategyMap = new HashMap<>();
+        strategyMap.put("register",new RegisterAuthCodeStrategy());
+        return strategyMap;
+    }
+
+    @Bean
+    public AuthCodeGenerator authCodeGenerator(){
+        return new UUIDAuthCodeGenerator();
+    }
+
+    @Bean
+    public Map<String,AuthCodeSender> authCodeSender(){
+        Map<String, AuthCodeSender> authCodeSenderMap = new HashMap<>();
+        authCodeSenderMap.put("email",new EmailAuthCodeSender(emailService,springTemplateEngine, authCodeProperties));
+        return authCodeSenderMap;
+    }
+
+    @Bean
+    public AuthCodeStorage authCodeStorage(){
+        return new RedisAuthCodeStorage(redisRepository,authCodeProperties);
+    }
+
+}
