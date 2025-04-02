34c7f08796d807ae5a0012cc0a83b8765020143b
pyunye <dpwns2318@naver.com>
2025-04-02 23:43:11 +0900

    [front] feat : login - 회원로그인 구현 마무리(api 호출 제외)

--- front/src/login/member/memberLogin.js
+++ front/src/login/member/memberLogin.js
@@ -1,10 +1,61 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './memberLogin.css'
+import "../../styles/styles.css"
 import SubHeader from "../../components/subHeader/subHeader";
+import locker from "../../styles/images/locker.png"
+import {Link} from "react-router-dom";
+import apiClient from "../../templates/apiClient";
 const MemberLogin = () => {
+    const [userId, setUserId] = useState('')
+    const [password, setPassword] = useState('')
+    function handleSignup() {
+        /*----------rest api------------
+        apiClient.post('/api/auth/login', {username: userId, password})
+         */
+    }
     return (
         <div className="login-page-container">
             <SubHeader />
+            <form onSubmit={handleSignup} className="login-content-container">
+                <div className="login-content-image">
+                    <img src={locker} className="locker-image"></img>
+                </div>
+                <div className="login-content-text">
+                <p className="login-title">회원 로그인</p>
+                    <div className="login-body">
+                        <div className="login-input-field">
+                            <p className="login-input-title">아이디</p>
+                            <input className="login-input-body"
+                                   type="text"
+                                   value={userId}
+                                   onChange={e => setUserId(e.target.value)}
+                                   required
+                                   />
+                        </div>
+                        <div className="login-input-field">
+                            <p className="login-input-title">비밀번호</p>
+                            <input className="login-input-body"
+                                   type="password"
+                                   value={password}
+                                   onChange={e => setPassword(e.target.value)}
+                                   required
+                            />
+                            <div className="login-forgotpw">
+                                <p className="login-forgotpw-text">비밀번호 찾기</p>
+                            </div>
+                            <button type="submit" className="login-button">로그인</button>
+                        </div>
+                    </div>
+                    <div className="login-underline"></div>
+                    <div className="login-joinmessage">
+                        <p className="login-joinmessage-text">
+                            아직 회원가입을 안하셨나요?</p>
+                        <Link to="/join" className="login-joinmessage-hyper" style={{color: 'blue'}}>
+                            회원가입
+                        </Link>
+                    </div>
+                </div>
+            </form>
         </div>
     );
 }
