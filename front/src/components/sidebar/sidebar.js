fc50e16a6a70072fd14797446c09998a928f2588
pyunye <dpwns2318@naver.com>
2025-04-03 18:41:40 +0900

    [front] feat : sidebar 구현

---
+++ front/src/components/sidebar/sidebar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './sidebar.css'
+import '../../styles/styles.css'
+import {Link, useLocation} from 'react-router-dom'
+
+const Sidebar = () => {
+    const location = useLocation()
+
+    return (
+        <div className="sidebar-container">
+            <p className="sidebar-title">시험 정보</p>
+            <div className="sidebar-content">
+                <Link to="/test/info" className={`sidebar-text ${location.pathname === "/test/info" ? "active" : ""}`}>
+                    예선시험 안내
+                </Link>
+                <Link to="/" className="sidebar-text">예선시험 접수</Link>
+            </div>
+            <div className="sidebar-underline"></div>
+            <p className="sidebar-title">문제 풀기</p>
+            <div className="sidebar-content">
+                <Link to = "/" className="sidebar-text">예선문제 풀기</Link>
+                <Link t0 = "/" className="sidebar-text">연습문제 풀기</Link>
+            </div>
+            <div className="sidebar-underline"></div>
+            <p className="sidebar-title">지원</p>
+            <div className="sidebar-content">
+                <Link to = "/" className="sidebar-text">증명서 발급</Link>
+                <Link to = "/" className="sidebar-text">QnA</Link>
+            </div>
+
+        </div>
+    )
+}
+
+export default Sidebar
