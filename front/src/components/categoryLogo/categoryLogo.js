9d81225d89ca02446e40a11ca8dfea977ad1f394
pyunye <dpwns2318@naver.com>
2025-04-03 20:09:39 +0900

    [front] feat : categoryLogo - 세부 페이지 로고 컴포넌트 구현

---
+++ front/src/components/categoryLogo/categoryLogo.js
@@ -0,0 +1,20 @@
+import './categoryLogo.css'
+import '../../styles/styles.css'
+import React from 'react'
+import trophy from '../../styles/images/test_info_logo.png'
+
+const CategoryLogo = ({logoTitle="예선시험 안내", imgSrc=trophy}) => {
+    return (
+        <div className="categoryLogo-container">
+            <div className="categoryLogo-textbox">
+                <p className="categoryLogo-title">{logoTitle}</p>
+                <p className="categoryLogo-subtitle">Creative Problem Solving Festival에 오신 것을 환영합니다.</p>
+            </div>
+            <div className="categoryLogo-imagebox">
+                <img className="categoryLogo-image" src={imgSrc} alt="logo-image"/>
+            </div>
+        </div>
+    )
+}
+
+export default CategoryLogo;
