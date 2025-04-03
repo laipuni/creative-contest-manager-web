b6798152ba2966edd595d3d6fc8348b1b507a908
laipuni <2tgb02023@gmail.com>
2025-04-03 14:07:07 +0900

    [back] test : 아이디 찾기 api 테스트

--- back/src/test/java/com/example/cpsplatform/auth/controller/AuthCodeControllerTest.java
+++ back/src/test/java/com/example/cpsplatform/auth/controller/AuthCodeControllerTest.java
@@ -2,12 +2,17 @@ package com.example.cpsplatform.auth.controller;
 
 import com.example.cpsplatform.auth.AuthService;
 import com.example.cpsplatform.auth.controller.request.AuthCodeSendRequest;
+import com.example.cpsplatform.auth.controller.request.FindIdRequest;
+import com.example.cpsplatform.auth.controller.response.FindIdResponse;
 import com.example.cpsplatform.member.repository.MemberRepository;
+import com.example.cpsplatform.member.service.RegisterService;
+import com.example.cpsplatform.member.service.dto.FindIdDto;
 import com.example.cpsplatform.security.config.SecurityConfig;
 import com.example.cpsplatform.security.service.LoginFailService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
@@ -17,13 +22,14 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Import(SecurityConfig.class)
-@WebMvcTest(AuthCodeController.class)
+@WebMvcTest(AuthController.class)
 class AuthCodeControllerTest {
 
     @MockitoBean
@@ -43,6 +49,10 @@ class AuthCodeControllerTest {
 
     @MockitoBean
     LoginFailService loginFailService;
+
+    @MockitoBean
+    RegisterService registerService;
+
 
 
     @DisplayName("인증코드를 수령할 사람의 정보, 전송 수단, 인증 수단을 값을 받는다.")
@@ -64,10 +74,14 @@ class AuthCodeControllerTest {
                         .content(content)
         )
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.code").value(200))
+                .andExpect(jsonPath("$.data").isEmpty());
+
     }
 
-    @DisplayName("인증코드를 수령할 사람의 정보, 전송 수단, 인증 수단을 값을 받는다.")
+    @DisplayName("인증코드를 수령할 사람의 정보가 없을 경우 예외로 응답한다.")
     @Test
     void sendAuthCodeWithBlankRecipient() throws Exception {
         //given
@@ -93,7 +107,7 @@ class AuthCodeControllerTest {
 
     }
 
-    @DisplayName("인증코드를 수령할 사람의 정보, 전송 수단, 인증 수단을 값을 받는다.")
+    @DisplayName("유효하지 않은 전송 수단일 경우 예외를 발생한다.")
     @Test
     void sendAuthCodeWithBlankSenderType() throws Exception {
         //given
@@ -119,7 +133,7 @@ class AuthCodeControllerTest {
 
     }
 
-    @DisplayName("인증코드를 수령할 사람의 정보, 전송 수단, 인증 수단을 값을 받는다.")
+    @DisplayName("유효하지 않은 인증 수단일 경우 예외로 응답한다.")
     @Test
     void sendAuthCodeWithBlankStrategyType() throws Exception {
         //given
@@ -142,6 +156,118 @@ class AuthCodeControllerTest {
                 .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
                 .andExpect(jsonPath("$.code").value(400))
                 .andExpect(jsonPath("$.message").value("인증 수단은 필수입니다."));
+
+    }
+
+    @DisplayName("아이디 찾기 요청을 받았을 때, 인증코드를 수령할 사람의 정보가 없을 경우 예외로 응답한다.")
+    @Test
+    void findLoginIdWithNotRecipient() throws Exception {
+        //given
+        String recipient = "";
+        String authCode = "authCode";
+        String senderType = "findId";
+        FindIdRequest request = new FindIdRequest(recipient,authCode,senderType);
+        String content = objectMapper.writeValueAsString(request);
+
+        //when
+        //then
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/api/v1/find-id")
+                                .with(csrf())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(content)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("인증 정보는 필수입니다."));
+
+    }
+
+    @DisplayName("아이디 찾기 요청을 받았을 때, 인증코드가 없을 경우 예외로 응답한다.")
+    @Test
+    void findLoginIdWithNotAuthCode() throws Exception {
+        //given
+        String recipient = "email@email.com";
+        String authCode = "";
+        String senderType = "findId";
+        FindIdRequest request = new FindIdRequest(recipient,authCode,senderType);
+        String content = objectMapper.writeValueAsString(request);
+
+        //when
+        //then
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/api/v1/find-id")
+                                .with(csrf())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(content)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("인증 코드는 필수입니다."));
+
+    }
+
+    @DisplayName("아이디 찾기 요청을 받았을 때, 인증코드를 수령할 사람의 정보가 없을 경우 예외로 응답한다.")
+    @Test
+    void findLoginIdWithNotSenderType() throws Exception {
+        //given
+        String recipient = "email@email.com";
+        String authCode = "authCode";
+        String senderType = "";
+        FindIdRequest request = new FindIdRequest(recipient,authCode,senderType);
+        String content = objectMapper.writeValueAsString(request);
+
+        //when
+        //then
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/api/v1/find-id")
+                                .with(csrf())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(content)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("전송 수단 선택은 필수입니다."));
+
+    }
+
+    @DisplayName("인증코드 수령한 사람의 정보, 인증코드, 전송수단을 받아 해당 정보와 일치하는 유저의 아이디를 반환한다.")
+    @Test
+    void findLoginId() throws Exception {
+        //given
+        String recipient = "email@email.com";
+        String authCode = "authCode";
+        String senderType = "findId";
+        String loginId = "testId";
+
+        FindIdRequest request = new FindIdRequest(recipient,authCode,senderType);
+        String content = objectMapper.writeValueAsString(request);
+
+        FindIdResponse response = FindIdResponse.builder()
+                .loginId(loginId)
+                .build();
+        when(registerService.findId(Mockito.any(FindIdDto.class)))
+                .thenReturn(response);
+
+        //when
+        //then
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/api/v1/find-id")
+                                .with(csrf())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(content)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.code").value(200))
+                .andExpect(jsonPath("$.data.loginId").value(response.getLoginId()));
 
     }
 
--- back/src/test/java/com/example/cpsplatform/member/service/RegisterServiceTest.java
+++ back/src/test/java/com/example/cpsplatform/member/service/RegisterServiceTest.java
@@ -1,28 +1,48 @@
 package com.example.cpsplatform.member.service;
 
+import com.example.cpsplatform.auth.AuthService;
+import com.example.cpsplatform.auth.controller.response.FindIdResponse;
 import com.example.cpsplatform.exception.PasswordMismatchException;
 import com.example.cpsplatform.member.controller.request.MemberRegisterReqeust;
 import com.example.cpsplatform.member.domain.Address;
 import com.example.cpsplatform.member.domain.Gender;
+import com.example.cpsplatform.member.domain.Member;
+import com.example.cpsplatform.member.domain.Role;
 import com.example.cpsplatform.member.domain.organization.school.School;
 import com.example.cpsplatform.member.domain.organization.school.StudentType;
+import com.example.cpsplatform.member.repository.MemberRepository;
+import com.example.cpsplatform.member.service.dto.FindIdDto;
 import com.example.cpsplatform.member.service.dto.RegisterRequestDto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.util.StringUtils;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
 
 @SpringBootTest
 class RegisterServiceTest {
 
     @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
     RegisterService registerService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @MockitoBean
+    AuthService authService;
+
 
     @DisplayName("비밀번호 확인과 비밀번호가 다를 경우, 예외가 발생한다.")
     @Test
@@ -50,6 +70,43 @@ class RegisterServiceTest {
         Assertions.assertThatThrownBy(() ->registerService.register(request))
                 .isInstanceOf(PasswordMismatchException.class)
                 .hasMessageMatching("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+    }
+
+    @DisplayName("이메일 인증 방식으로 아이디를 찾을 경우 이메일로 회원의 아이디를 조회한다.")
+    @Test
+    void findId(){
+        //given
+        Address address = new Address("street","zipCode","detail");
+        School school = new School("xx대학교", StudentType.COLLEGE,4);
+        String email = "email@email.com";
+        Member member = Member.builder()
+                .loginId("loginId")
+                .password(passwordEncoder.encode("1234"))
+                .role(Role.USER)
+                .birth(LocalDate.now())
+                .email(email)
+                .address(address)
+                .gender(Gender.MAN)
+                .phoneNumber("01012341234")
+                .name("사람 이름")
+                .organization(school)
+                .build();
+        memberRepository.save(member);
+
+
+
+        String authCode = "1234";
+        FindIdDto findIdDto = new FindIdDto(email, authCode,"email");
+
+        Mockito.when(authService.verifyAuthCode(anyString(),anyString(),anyString()))
+                .thenReturn(true);
+
+        //when
+        FindIdResponse result = registerService.findId(findIdDto);
+
+        //then
+        Assertions.assertThat(result.getLoginId()).isNotNull()
+                .isEqualTo(member.getLoginId());
     }
 
 }
