af6320b7c2018a46790507a3de67d16151a1bfb8
pyunye <dpwns2318@naver.com>
2025-04-03 18:42:08 +0900

    [front] feat : test - testInfo 틀 구현

--- front/src/testRegister/testInfo/testInfo.js
+++ front/src/testRegister/testInfo/testInfo.js
@@ -1,11 +1,15 @@
 import './testInfo.css'
 import React from 'react'
 import MainHeader from "../../components/mainHeader/mainHeader";
+import Sidebar from "../../components/sidebar/sidebar";
 
 const TestInfo = () => {
     return (
         <div className="testInfo-page-container">
-            <MainHeader />
+            <MainHeader underbarWidth="100%"/>
+            <div className="testInfo-content-container">
+                <Sidebar />
+            </div>
         </div>
     )
 }
