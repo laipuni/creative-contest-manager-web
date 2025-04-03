51edfae5cf0bc641456cf1ac095c99bd2640599b
laipuni <2tgb02023@gmail.com>
2025-04-03 10:24:15 +0900

    [back] feat : 회원 단건 조회 fetch join 적용 및 이메일로 회원 정보찾기 기능 추가

--- back/src/main/java/com/example/cpsplatform/member/repository/MemberRepository.java
+++ back/src/main/java/com/example/cpsplatform/member/repository/MemberRepository.java
@@ -1,12 +1,15 @@
 package com.example.cpsplatform.member.repository;
 
 import com.example.cpsplatform.member.domain.Member;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
 
+    @EntityGraph(attributePaths = "organization")
     Optional<Member> findMemberByLoginId(String loginId);
-
+    @EntityGraph(attributePaths = "organization")
+    Optional<Member> findMemberByEmail(String email);
 }
