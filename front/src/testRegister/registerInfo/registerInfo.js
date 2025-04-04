a8c1417ae288af271a69749b3d86e8fa9c668081
pyunye <dpwns2318@naver.com>
2025-04-04 13:41:11 +0900

    [front] feat : test - 예선 접수내역 페이지 js 구현

--- front/src/testRegister/registerInfo/registerInfo.js
+++ front/src/testRegister/registerInfo/registerInfo.js
@@ -4,6 +4,7 @@ import MainHeader from "../../components/mainHeader/mainHeader";
 import Sidebar from "../../components/sidebar/sidebar";
 import CategoryLogo from "../../components/categoryLogo/categoryLogo";
 import trophyLogo from "../../styles/images/test_info_logo.png";
+import {Link} from "react-router-dom";
 
 const registerInfo = () => {
     return (
@@ -14,6 +15,33 @@ const registerInfo = () => {
                     <Sidebar/>
                     <div className="testInfo-main-container">
                         <CategoryLogo logoTitle={"예선시험 접수"} imgSrc={trophyLogo}/>
+                        <div className="registerInfo-body-container">
+                            <div className="registerInfo-body-top">
+                                <p className="registerInfo-top-title">접수 내역</p>
+                                <div className="registerInfo-underline"></div>
+                            </div>
+                            <div className="registerInfo-body-bot">
+                                <div className="registerInfo-bot-title">
+                                    <p className="registerInfo-bot-text">팀명</p>
+                                    <div className="registerInfo-bot-line"></div>
+                                    <p className="registerInfo-bot-text">접수번호</p>
+                                    <div className="registerInfo-bot-line"></div>
+                                    <p className="registerInfo-bot-text">접수일자</p>
+                                    <div className="registerInfo-bot-line"></div>
+                                    <p className="registerInfo-bot-text">팀장(이메일)</p>
+                                </div>
+                                <div className="registerInfo-bot-content">
+                                    <p className="registerInfo-bot-text">팀명</p>
+                                    <p className="registerInfo-bot-text">팀명</p>
+                                    <p className="registerInfo-bot-text">팀명</p>
+                                    <p className="registerInfo-bot-text">팀명</p>
+
+                                </div>
+                                <div className="registerInfo-bot-buttonbox">
+                                    <Link to="/" className="registerInfo-bot-button">접수하기</Link>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
