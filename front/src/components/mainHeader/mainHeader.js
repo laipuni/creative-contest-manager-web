6af09956ddc609f4256e95050c3fe3f730100e0e
pyunye <dpwns2318@naver.com>
2025-04-10 21:07:36 +0900

    [front] feat : mainHeader - 헤더 로그아웃 구현

--- front/src/components/mainHeader/mainHeader.js
+++ front/src/components/mainHeader/mainHeader.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './mainHeader.css'
 import '../../styles/styles.css'
 import logo from '../../styles/images/trophy.png'
 import { Link } from 'react-router-dom'
+import apiClient from "../../templates/apiClient";
+import axios from "axios";
 const MainHeader = ({underbarWidth = "75%"}) => {
+    const [isAuthenticated, setIsAuthenticated] = useState(localStorage.getItem("isAuthenticated"));
+    function handleLogout() {
+        apiClient.post('/api/auth/logout')
+            .then((res)=>{
+                localStorage.removeItem("isAuthenticated");
+                setIsAuthenticated(null);
+                //csrf 토큰 다시 얻어오기
+                axios.get("/api/csrf")
+                    .then((data) => {
+                    })
+                    .catch((error) => {alert(error.message)});
+            })
+            .catch((err)=>{})
+    }
+
     return (
         <div className="main-header-container">
             <div className="main-header-top">
@@ -12,23 +29,41 @@ const MainHeader = ({underbarWidth = "75%"}) => {
                         <Link to="/" className="main-header-menu-item-text">HOME</Link>
                         <div className="main-header-menu-item-line"></div>
                     </div>
-                    <div className="main-header-menu-item">
-                        <Link to="/member/login" className="main-header-menu-item-text">LOGIN</Link>
-                        <div className="main-header-menu-item-line"></div>
-                    </div>
-                    <div className="main-header-menu-item">
-                        <Link to="/join/policy" className="main-header-menu-item-text">JOIN</Link>
-                        <div className="main-header-menu-item-line"></div>
-                    </div>
-                    <div className="main-header-menu-item">
-                        <Link to="/admin/login" className="main-header-menu-item-text">ADMIN</Link>
-                    </div>
+                    {isAuthenticated !== 'true' &&
+                        <>
+                            <div className="main-header-menu-item">
+                                <Link to="/member/login" className="main-header-menu-item-text">LOGIN</Link>
+                                <div className="main-header-menu-item-line"></div>
+                            </div>
+                            <div className="main-header-menu-item">
+                            <Link to="/join/policy" className="main-header-menu-item-text">JOIN</Link>
+                            <div className="main-header-menu-item-line"></div>
+                            </div>
+                            <div className="main-header-menu-item">
+                            <Link to="/admin/login" className="main-header-menu-item-text">ADMIN</Link>
+                            </div>
+                        </>}
+                    {isAuthenticated === 'true' &&
+                        <>
+                            <div className="main-header-menu-item">
+                                <p onClick={handleLogout} style={{cursor: 'pointer'}}
+                                   className="main-header-menu-item-text">LOGOUT</p>
+                                <div className="main-header-menu-item-line"></div>
+                            </div>
+                            <div className="main-header-menu-item">
+                                <Link to="/join/policy" className="main-header-menu-item-text">JOIN</Link>
+                                <div className="main-header-menu-item-line"></div>
+                            </div>
+                            <div className="main-header-menu-item">
+                                <Link to="/admin/login" className="main-header-menu-item-text">ADMIN</Link>
+                            </div>
+                        </>}
                 </div>
             </div>
             <div className="main-header-bot">
                 <Link to="/" className="main-header-logo">
                     <img src={logo} alt="logo" className="main-logo-image"/>
-                    <p className="main-logo-text">Creative Problem<br />Solving Festival</p>
+                    <p className="main-logo-text">Creative Problem<br/>Solving Festival</p>
                 </Link>
                 <div className="main-header-bot-right">
                     <div className="main-header-bot-textbox">
