89ec563995224a6cac56ed654da7f9a9ac7f19ef
pyunye <dpwns2318@naver.com>
2025-04-11 20:29:23 +0900

    [front] feat : categoryLogo - 로고 컴포넌트 배경색, 이미지 크기 속성 추가

--- front/src/components/categoryLogo/categoryLogo.js
+++ front/src/components/categoryLogo/categoryLogo.js
@@ -3,14 +3,14 @@ import '../../styles/styles.css'
 import React from 'react'
 import trophy from '../../styles/images/test_info_logo.png'
 
-const CategoryLogo = ({logoTitle, imgSrc}) => {
+const CategoryLogo = ({logoTitle, imgSrc, backgroundColor, imageWidth}) => {
     return (
-        <div className="categoryLogo-container">
+        <div className="categoryLogo-container" style={{background: backgroundColor}}>
             <div className="categoryLogo-textbox">
                 <p className="categoryLogo-title">{logoTitle}</p>
                 <p className="categoryLogo-subtitle">Creative Problem Solving Festival에 오신 것을 환영합니다.</p>
             </div>
-            <div className="categoryLogo-imagebox">
+            <div className="categoryLogo-imagebox" style={{width: imageWidth}}>
                 <img className="categoryLogo-image" src={imgSrc} alt="logo-image"/>
             </div>
         </div>
