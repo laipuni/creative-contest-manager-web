2e29dcbbbddedc8adf9e8fccc0bf656490ffefed
laipuni <2tgb02023@gmail.com>
2025-04-14 12:52:52 +0900

    [back] feat : s3 의존성 추가, s3 서비스 및 스토리지 추가

---
+++ back/src/main/java/com/example/cpsplatform/file/storage/S3FileStorage.java
@@ -0,0 +1,30 @@
+package com.example.cpsplatform.file.storage;
+
+
+import com.example.cpsplatform.file.decoder.vo.FileSources;
+import com.example.cpsplatform.s3.S3Service;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class S3FileStorage implements FileStorage {
+
+    private final S3Service s3Service;
+
+    public S3FileStorage(final S3Service s3Service) {
+        this.s3Service = s3Service;
+    }
+
+    @Override
+    public void upload(final String path, final FileSources fileSources) {
+        fileSources.getFileSourceList().forEach(fileSource -> {
+            s3Service.upload(
+                    path,
+                    fileSource.getUploadFileName(),
+                    fileSource.getFileBytes(),
+                    fileSource.getMimeType(),
+                    fileSource.getExtension(),
+                    fileSource.getSize()
+            );
+        });
+    }
+}
