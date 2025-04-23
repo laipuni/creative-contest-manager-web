970f4789150fd6be397a1baf839e57423122e9cb
laipuni <2tgb02023@gmail.com>
2025-04-23 20:12:10 +0900

    [back] feat : team solve 엔티티 추가
    team solve엔티티는 team - problem의 관계를 일대다 다대일로 풀어내기 위해 만든 중간 엔티티이다. 주로 수정, 답안지 파일 참조를 맡고있다.

---
+++ back/src/main/java/com/example/cpsplatform/teamsolve/repository/TeamSolveRepository.java
@@ -0,0 +1,7 @@
+package com.example.cpsplatform.teamsolve.repository;
+
+import com.example.cpsplatform.teamsolve.domain.TeamSolve;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamSolveRepository extends JpaRepository<TeamSolve,Long> {
+}
