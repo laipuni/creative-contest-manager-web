5183444217194b0ee7575f1b1cd99dec657ad6e7
pyunye <dpwns2318@naver.com>
2025-04-25 16:57:49 +0900

    [front] feat : adminSidebar - 관리자 페이지 사이드바 구현

---
+++ front/src/admin/components/adminSidebar/adminSidebar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './adminSidebar.css'
+import {Link, useLocation} from "react-router-dom";
+
+const AdminSidebar = () => {
+    const location = useLocation();
+    //TODO- 예선 관리, 데이터 분석 페이지 링크 연동
+    return (
+        <div className="adminSidebar-container">
+            <Link to="/admin/teamList"
+                  className={`adminSidebar-category ${location.pathname === "/admin/teamList" ? "active" : ""}`}>
+                <p className="adminSidebar-text">팀 목록</p>
+            </Link>
+            <div className="adminSidebar-category">
+                <p className="adminSidebar-text">예선 관리</p>
+            </div>
+            <div className="adminSidebar-category">
+                <p className="adminSidebar-text">데이터 분석</p>
+            </div>
+        </div>
+    )
+}
+
+export default AdminSidebar;
