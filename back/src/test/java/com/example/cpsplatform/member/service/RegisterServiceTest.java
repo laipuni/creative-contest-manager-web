89ca0b4011bb3d66a885d3f33da1c896222800ac
laipuni <2tgb02023@gmail.com>
2025-04-28 12:34:00 +0900

    [back] test : RegisterServiceTest 테스트 완료 후 데이터 초기화 로직 추가

--- back/src/test/java/com/example/cpsplatform/member/service/RegisterServiceTest.java
+++ back/src/test/java/com/example/cpsplatform/member/service/RegisterServiceTest.java
@@ -14,6 +14,7 @@ import com.example.cpsplatform.member.repository.MemberRepository;
 import com.example.cpsplatform.auth.service.dto.FindIdDto;
 import com.example.cpsplatform.auth.service.dto.RegisterRequestDto;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -42,7 +43,7 @@ class RegisterServiceTest {
     @MockitoBean
     AuthService authService;
 
-    @BeforeEach
+    @AfterEach
     void tearUp(){
         memberRepository.deleteAll();
     }
