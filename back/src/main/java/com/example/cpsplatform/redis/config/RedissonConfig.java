03fce24c7b00c4d239ffc42a52564970d41cb5e3
laipuni <2tgb02023@gmail.com>
2025-04-28 16:44:02 +0900

    [back] refactor : redis tls 설정 및 redisson 호스트, 포트 설정 변경

--- back/src/main/java/com/example/cpsplatform/redis/config/RedissonConfig.java
+++ back/src/main/java/com/example/cpsplatform/redis/config/RedissonConfig.java
@@ -1,21 +1,29 @@
 package com.example.cpsplatform.redis.config;
 
+
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
 import org.redisson.codec.JsonJacksonCodec;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
         //Redis 서버 연결 설정
         config.useSingleServer()
-                .setAddress("redis://localhost:6379");
+                .setAddress(String.format("redis://%s:%d",redisHost,redisPort));
         //직렬화 Jackson JSON 방식 설정
         config.setCodec(new JsonJacksonCodec());
         return Redisson.create(config);
