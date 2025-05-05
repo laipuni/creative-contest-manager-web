d942f0d461e18756cabc3fb166e228f347829dc2
pyunye <dpwns2318@naver.com>
2025-05-05 20:38:25 +0900

    [front] feat : registerInfo - 팀 접수내역 api 연동

--- front/src/testRegister/registerInfo/registerInfo.js
+++ front/src/testRegister/registerInfo/registerInfo.js
@@ -9,41 +9,25 @@ import {format} from 'date-fns'
 import apiClient from "../../templates/apiClient";
 import rocket from "../../styles/images/solve_icon.png";
 
-//출력 예시
-const exampleData = {
-    teamName: "챌린저 팀",
-    registerNum: "20250404-001",
-    registerDate: "2025-04-04T15:00:00.000Z",
-    leaderName: "김철수",
-    leaderEmail: "leader@example.com"
-};
-
 const RegisterInfo = () => {
-    const [teamName, setTeamName] = useState('');
-    const [registerNum, setRegisterNum] = useState('');
-    const [registerDate, setRegisterDate] = useState('');
-    const [leaderName, setLeaderName] = useState('');
-    const [leaderEmail, setLeaderEmail] = useState('');
+    const [teamInfo, setTeamInfo] = useState(null)
 
     useEffect(() => {
-        setTeamName(exampleData.teamName);
-        setRegisterNum(exampleData.registerNum);
-        setRegisterDate(format(new Date(exampleData.registerDate), 'yyyy-MM-dd'));
-        setLeaderName(exampleData.leaderName);
-        setLeaderEmail(exampleData.leaderEmail);
+        apiClient.get('/api/contests/latest')
+            .then((res)=>{
+                if(res.data.data){
+                    apiClient.get(`/api/contests/${res.data.data.contestId}/team`, {skipErrorHandler: true})
+                        .then((res) => {
+                            setTeamInfo(res.data.data)
+                        })
+                        .catch((err)=>{
+                            if(err.response.status !== 400) alert(err.response.data.message);
+                        })
+                }
+            })
+            .catch((err)=>{})
     }, []);
 
-    /*-----------------접수 내역 가져오기---------
-    apiClient.get('/api/register/info'})
-        .then((res)=>{
-            setTeamName(res.data.teamName);
-            setRegisterNum(res.data.registerNum);
-            setRegisterDate(format(new Date(res.data.registerDate), 'yyyy-MM-dd'))
-            setLeaderName(res.data.leaderName)
-            setLeaderEmail(res.data.leaderEmail)
-        });
-
-     */
 
     return (
         <div className="testInfo-page-container">
@@ -62,17 +46,22 @@ const RegisterInfo = () => {
                                 <div className="registerInfo-bot-title">
                                     <p className="registerInfo-bot-text">팀명</p>
                                     <div className="registerInfo-bot-line"></div>
-                                    <p className="registerInfo-bot-text">접수번호</p>
+                                    <p className="registerInfo-bot-text">팀원1</p>
+                                    <div className="registerInfo-bot-line"></div>
+                                    <p className="registerInfo-bot-text">팀원2</p>
                                     <div className="registerInfo-bot-line"></div>
                                     <p className="registerInfo-bot-text">접수일자</p>
                                     <div className="registerInfo-bot-line"></div>
                                     <p className="registerInfo-bot-text">팀장(이메일)</p>
                                 </div>
-                                {teamName && <div className="registerInfo-bot-content">
-                                    <p className="registerInfo-bot-text">{teamName}</p>
-                                    <p className="registerInfo-bot-text">{registerNum}</p>
-                                    <p className="registerInfo-bot-text">{registerDate}</p>
-                                    <p className="registerInfo-bot-text">{leaderName}({leaderEmail})</p>
+                                {teamInfo && <div className="registerInfo-bot-content">
+                                    <p className="registerInfo-bot-text">{teamInfo.teamName}</p>
+                                    {teamInfo.memberIds.filter(id => id !== teamInfo.leader.loginId)
+                                        .map((id, index) => (
+                                        <p className="registerInfo-bot-text" key={index+1}>{id}</p>
+                                    ))}
+                                    <p className="registerInfo-bot-text">{format(new Date(teamInfo.createdAt), 'yyyy-MM-dd')}</p>
+                                    <p className="registerInfo-bot-text">{teamInfo.leader.name}({teamInfo.leader.email})</p>
                                 </div>}
                                 <div className="registerInfo-bot-buttonbox">
                                     <Link to="/register/team" className="registerInfo-bot-button">
