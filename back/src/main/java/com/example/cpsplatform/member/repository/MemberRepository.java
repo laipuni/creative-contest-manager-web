6ea717a55f3407d2f1f75173edde53493711dde9
laipuni <2tgb02023@gmail.com>
2025-05-09 10:45:36 +0900

    [back] refactor : 아이디, 이메일로 계정 찾기를 관리자가 아닌 유저만 찾을 수 있게 변경

--- back/src/main/java/com/example/cpsplatform/member/repository/MemberRepository.java
+++ back/src/main/java/com/example/cpsplatform/member/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package com.example.cpsplatform.member.repository;
 
 import com.example.cpsplatform.member.domain.Member;
+import com.example.cpsplatform.member.domain.Role;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,11 +11,18 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @EntityGraph(attributePaths = "organization")
     Optional<Member> findMemberByLoginId(String loginId);
-    @EntityGraph(attributePaths = "organization")
-    Optional<Member> findMemberByEmail(String email);
 
+    //관리자가 아닌 아이디와 일치하는 유저찾는 쿼리
     @EntityGraph(attributePaths = "organization")
-    Optional<Member> findMemberByEmailAndLoginId(String email,String loginId);
+    Optional<Member> findMemberByLoginIdAndRole(String loginId,Role role);
+
+    //관리자가 아닌 이메일과 일치하는 유저 찾는 쿼리
+    @EntityGraph(attributePaths = "organization")
+    Optional<Member> findMemberByEmailAndRole(String email,Role role);
+
+    //관리자가 아닌 이메일과 아이디가 일치하는 유저 찾는 쿼리
+    @EntityGraph(attributePaths = "organization")
+    Optional<Member> findMemberByEmailAndLoginIdAndRole(String email,String loginId, Role role);
 
     boolean existsByLoginId(String LoginId);
 
