2420721a0316ec8371404612b74583deb8679c3d
laipuni <2tgb02023@gmail.com>
2025-05-17 13:59:31 +0900

    [back] feat : 관리자용 회원 상세 정보 조회 api 테스트 추가

--- back/src/main/java/com/example/cpsplatform/member/admin/MemberAdminService.java
+++ back/src/main/java/com/example/cpsplatform/member/admin/MemberAdminService.java
@@ -1,20 +1,35 @@
 package com.example.cpsplatform.member.admin;
 
+import com.example.cpsplatform.member.admin.controller.response.MemberDetailInfoResponse;
 import com.example.cpsplatform.member.admin.controller.response.MemberInfoListResponse;
+import com.example.cpsplatform.member.domain.Member;
 import com.example.cpsplatform.member.repository.MemberRepository;
 import com.example.cpsplatform.member.repository.dto.AdminMemberSearchCond;
+import com.example.cpsplatform.security.encoder.CryptoService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MemberAdminService {
 
+    public static final String MEMBER_ADMIN_LOG = "[MemberAdminService]";
+
     private final MemberRepository memberRepository;
+    private final CryptoService cryptoService;
 
     public MemberInfoListResponse searchMember(final AdminMemberSearchCond cond) {
         return memberRepository.searchMemberByAdminCond(cond);
+    }
+
+    public MemberDetailInfoResponse getMemberDetailInfo(final Long memberId) {
+        log.info("{} 유저(id:{})의 정보를 조회",MEMBER_ADMIN_LOG,memberId);
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저는 존재하지 않습니다."));
+        return MemberDetailInfoResponse.of(member,cryptoService);
     }
 }
--- back/src/main/java/com/example/cpsplatform/member/admin/controller/MemberAdminController.java
+++ back/src/main/java/com/example/cpsplatform/member/admin/controller/MemberAdminController.java
@@ -3,11 +3,13 @@ package com.example.cpsplatform.member.admin.controller;
 import com.example.cpsplatform.ApiResponse;
 import com.example.cpsplatform.admin.annotaion.AdminLog;
 import com.example.cpsplatform.member.admin.MemberAdminService;
+import com.example.cpsplatform.member.admin.controller.response.MemberDetailInfoResponse;
 import com.example.cpsplatform.member.admin.controller.response.MemberInfoListResponse;
 import com.example.cpsplatform.member.domain.Gender;
 import com.example.cpsplatform.member.repository.dto.AdminMemberSearchCond;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,4 +46,10 @@ public class MemberAdminController {
         return ApiResponse.ok(response);
     }
 
+    @AdminLog
+    @GetMapping("/api/admin/members/{memberId}")
+    public ApiResponse<MemberDetailInfoResponse> getMemberDetailInfo(@PathVariable("memberId") Long memberId){
+        MemberDetailInfoResponse response = memberAdminService.getMemberDetailInfo(memberId);
+        return ApiResponse.ok(response);
+    }
 }
