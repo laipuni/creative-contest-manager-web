823850872dce4f051f3e2bafb51a83b5ad2e788c
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2025-05-20 14:24:08 +0900

    [back] refactor : faq 연동 기능 api명 변경

--- back/src/main/java/com/example/cpsplatform/ai/controller/AiController.java
+++ back/src/main/java/com/example/cpsplatform/ai/controller/AiController.java
@@ -1,26 +1,21 @@
 package com.example.cpsplatform.ai.controller;
 
 import com.example.cpsplatform.ApiResponse;
-import com.example.cpsplatform.ai.admin.controller.request.QuestionGenerateRequest;
-import com.example.cpsplatform.ai.admin.service.dto.QuestionDto;
 import com.example.cpsplatform.ai.controller.request.FaqRequest;
 import com.example.cpsplatform.ai.controller.response.FaqResponse;
 import com.example.cpsplatform.ai.service.AiApiService;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/ai")
 @RequiredArgsConstructor
 public class AiController {
 
     private final AiApiService aiApiService;
 
-    @PostMapping("/qa")
+    @PostMapping("/api/questions")
     public ApiResponse<FaqResponse> generateFaq(@RequestBody FaqRequest request) {
         FaqResponse result = aiApiService.getAnswerFromFaqChatBot(request);
         return ApiResponse.ok(result);
