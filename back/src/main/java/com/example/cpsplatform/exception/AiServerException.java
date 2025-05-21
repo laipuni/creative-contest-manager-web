ec4d956885f97aa734185794696f2f6638d9cfbb
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2025-05-21 18:03:50 +0900

    [back] feat : ai 연동 기능 400, 500 에러 커스텀 예외 생성

--- back/src/main/java/com/example/cpsplatform/exception/AiServerException.java
+++ back/src/main/java/com/example/cpsplatform/exception/AiServerException.java
@@ -2,19 +2,11 @@ package com.example.cpsplatform.exception;
 
 public class AiServerException extends RuntimeException{
 
-    private final int status;
-
-    public AiServerException(final String message, final int status) {
+    public AiServerException(final String message) {
         super(message);
-        this.status = status;
     }
 
-    public AiServerException(final String message, int status, final Throwable cause) {
+    public AiServerException(final String message, final Throwable cause) {
         super(message, cause);
-        this.status = status;
-    }
-
-    public int getStatus() {
-        return status;
     }
 }
