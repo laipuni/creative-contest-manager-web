274c7b2ebc4c4d867e830afe156d5a24958fcfa6
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2025-05-21 18:09:41 +0900

    [back] refactor : request에 어노테이션(notblank, min)추가

--- back/src/main/java/com/example/cpsplatform/ai/controller/request/FaqRequest.java
+++ back/src/main/java/com/example/cpsplatform/ai/controller/request/FaqRequest.java
@@ -1,10 +1,13 @@
 package com.example.cpsplatform.ai.controller.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class FaqRequest {
+
+    @NotBlank(message = "질문은 필수입니다.")
     private String question;
 }
