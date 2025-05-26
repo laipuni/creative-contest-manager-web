e2e22e36888c1e3fef4592a377e31b427a16994f
laipuni <2tgb02023@gmail.com>
2025-05-26 21:38:29 +0900

    [back] refactor : Ip유틸, 어드민 로그 프록시 클래스 주석 추가

--- back/src/main/java/com/example/cpsplatform/ClientIpUtils.java
+++ back/src/main/java/com/example/cpsplatform/ClientIpUtils.java
@@ -5,8 +5,19 @@ import org.springframework.util.StringUtils;
 
 import java.util.Optional;
 
+/**
+ * 클라이언트의 실제 IP 주소를 추출하는 유틸리티 클래스입니다.
+ * 프록시(예: Nginx, AWS ELB 등)를 통해 전달된 요청의 경우,
+ * HttpServletRequest의 getRemoteAddr()은 프록시 서버의 IP를 반환합니다.
+ * 이 경우 "X-Forwarded-For" 헤더에 실제 클라이언트 IP가 포함되어 있으므로 해당 값을 우선적으로 사용
+ */
 public class ClientIpUtils {
 
+    /**
+     *  * X-Forwarded-For 헤더가 존재하면, 가장 앞에 있는 IP(실제 클라이언트 IP)를 반환
+     *  * 없으면 request.getRemoteAddr() 값을 반환
+     *  * IP 정보를 얻지 못하면 "unknown"을 반환
+     */
     public static String getClientIp(HttpServletRequest request) {
         String xff = request.getHeader("X-Forwarded-For");
         if (StringUtils.hasText(xff)) {
--- back/src/main/java/com/example/cpsplatform/admin/aop/AdminLogProxy.java
+++ back/src/main/java/com/example/cpsplatform/admin/aop/AdminLogProxy.java
@@ -12,6 +12,13 @@ import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
+
+/**
+ * 어드민 요청에 대한 로그를 기록하는 AOP 클래스
+ * @AdminLog 애너테이션이 붙은 컨트롤러 메서드에 대해 사전 처리(@Before)로 작동
+ * 관리자 요청의 URI, 요청자(username), IP 주소, 호출된 메서드명, 전달된 파라미터를 로그로 남김
+ * 로그는 운영 중 관리자 페이지의 요청 추적 및 감사 목적으로 활용
+ */
 @Slf4j
 @Aspect
 @Component
@@ -20,7 +27,6 @@ public class AdminLogProxy {
 
     private final HttpServletRequest request;
 
-    //@AdminLog가 있는 메서드들은 아래와 같은 로그를 남긴다.
     @Before("@annotation(com.example.cpsplatform.admin.annotaion.AdminLog)")
     public void logAdminReqeust(JoinPoint joinPoint){
         String ip = ClientIpUtils.getClientIp(request);
